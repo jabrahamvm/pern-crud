e033927b57a2d84a794c45887b0bcac44eeec7e6
jabrahamvm <abrahamvillalba00@gmail.com>
2023-08-24 15:29:30 -0700

    Added button to create a task

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
+import React from 'react';
+import InputTodo from './components/InputTodo';
+
 function App() {
 
   return (
     <>
       <h1>App</h1>
+      <InputTodo />
     </>
   )
 }
---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+// This component will be used to input new todos into the database.
+
+const InputTodo = () => {
+    const [description, setDescription] = useState("");
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            console.log(description);
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
+            console.log(response);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    return (
+        <form className="form-row mt-5" onSubmit={onSubmitForm}>
+            <div className="col-sm-10">
+                <input type="text" className="form-control" placeholder="Add todo..." onChange={(e) => {setDescription(e.target.value)}}/>
+            </div>
+            <div className="col-sm-2">
+                <button className="btn btn-success">Add</button>
+            </div>
+        </form>
+    )
+}
+
+export default InputTodo;
